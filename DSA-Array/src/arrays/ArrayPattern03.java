17d93c31dd1b9f9a67812efa5142549bf5827bfe
adityaraj3324 <asahu3324@gmail.com>
2025-05-26 13:54:38 +0530

    ArrayPattern03

---
+++ DSA-Array/src/arrays/ArrayPattern03.java
@@ -0,0 +1,41 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class ArrayPattern03{
+	public static int Max(int arr[]) {
+		int temp = arr[0];
+		for(int i=1;i<arr.length;i++) {
+			if(temp<arr[i]) {
+				temp = arr[i];
+			}
+		}
+		return temp;
+	}
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the number of elements: ");
+		int num = sc.nextInt();
+		System.out.println("Enter "+num+" elements.");
+		int arr[] = new int[num];
+		for(int i=0;i<num;i++) {
+			arr[i] = sc.nextInt();
+		}
+		int max = Max(arr);
+		
+		for(int i=0;i<max;i++) {
+			for(int j=0;j<num;j++) {
+				if(i<arr[j]) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+				
+			}
+			System.out.println();
+		}
+//		System.out.println(max);
+		sc.close();
+	}
+}
